7801e0b0cd7d28cc3579d4ab78494f2e75e2b502
Anwesh43 <anweshthecool0@gmail.com>
2017-02-25 17:08:33 +0530

    creating names by combining entry in firstnames and lastnames

--- lib/elixirfirstproject.ex
+++ lib/elixirfirstproject.ex
@@ -2,4 +2,11 @@ defmodule Elixirfirstproject do
     def hello do
       "hello world"
     end
+    def createNames do
+      lastNames = ["Krul","Srul","Mril","Johnson"]
+      firstNames = ["Jon","Brin","Trisk","Lorel"]
+      for firstName <- firstNames,lastName <- lastNames do
+        "Mr #{firstName} #{lastName}"
+      end
+    end
 end
